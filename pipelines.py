9b9327378c2f486fb1f81c2a15befe03658048f6
DADI91 <48893991+DADI91@users.noreply.github.com>
2020-06-15 17:25:04 +0200

    scrap_web

--- pipelines.py
+++ pipelines.py
@@ -4,3 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+from pymongo import MongoClient
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+from scrapy import log
+
+class ScrapWebPipeline(object):
+ 
+    def process_item(self, item, spider):
+    
+        return item
+        print(f"coucou + {item}")
+
